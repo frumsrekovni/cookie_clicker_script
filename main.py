4506804316f955c823fb7a56de6e112064c8e08c
frumsrekovni <joshua.backstrom@gmail.com>
2022-02-03 22:01:34 +0100

    Decision logic works as expected now. Implemented user input handling

--- main.py
+++ main.py
@@ -13,31 +13,47 @@ from selenium.webdriver.support import expected_conditions as EC
 # Effort is put towards decreasing the number of evals put still putting that number to use.
 # Its a balancing act of how often an eval should be done. If evals were as fast as a click then this would be a non-issue.
 
+
+
 def main():
-    if(input("[1]Classic or [2]New version?\n") == "1"):
-        classic = True
-    else:
-        classic = False
+    while True:
+        version = input("[1]Classic or [2]New?\n").lower()
+        if((version == "1") or (version == "classic")):
+            classic = True
+            break
+        elif((version == "2") or (version == "new")):
+            classic = False
+            break
+        else:
+            print("\nDo you want to play the classic cookie clicker or new cookie clicker?\nEnter 1 for Classic\nEnter 2 for New")
+    while True:
+        try:
+            inputText = input("How many cookie clicks until building purchase?\n") # Higher is longer time between shopping
+            if(not inputText.isdigit()):
+                raise ValueError("Error: Amount of clicks needs to be a valid number")
+            clicks_threshold = int(inputText)
+            if(classic):
+                break
+            else:
+                inputText = input("How many building purchases until another cps evaluation?\n")
+                if(inputText.isdigit()):
+                    number_of_purchases_per_eval = int(inputText)
+                    break
+                else:
+                    raise ValueError("Error: Purchases per evaluation needs to be a valid number, I can recommend it being in the single digits")
+        except ValueError as verr:
+            print(verr)
+            continue
 
-    try:
-        inputText = input("How many clicks until building purchase?\n") # Higher is longer time between shopping
-        if(not inputText.isdigit()):
-            raise
-        clicks_threshold = int(inputText)
-    except:
-        print("Not valid")
-        clicks_threshold = 500    
-
-    cookies_per_cps = 1000.0 # Less is better
+    cookies_per_cps = 100000.0 # Less is better
     clicks = 0
-    number_of_purchases_per_eval = 4
     number_of_purchases_per_eval_counter = 0
 
     ser = Service("C:\Program Files (x86)\chromedriver.exe")
     options = webdriver.ChromeOptions()
     options.add_argument("--incognito")
     driver = webdriver.Chrome(service=ser,options=options)
-    #driver.maximize_window()
+    driver.maximize_window()
     
     if(classic): # Open and play classic cookie clicker
         which_product_to_click = ["Cursor",cookies_per_cps]
@@ -45,10 +61,10 @@ def main():
         time.sleep(3)
         while(True):
             driver.find_element(By.XPATH, '//*[@id="cookie"]').click()
+            which_product_to_click[1] = -1.0
             clicks += 1
             if(clicks >= clicks_threshold):
                 clicks = 0
-                which_product_to_click[1] = cookies_per_cps
                 storeBuildings = driver.find_element(By.XPATH, '//*[@id="store"]').find_elements(By.TAG_NAME, 'DIV')
                 for building in storeBuildings:
                     outerTextOfBuildingList = building.get_attribute("outerText").split("\n",1)[0]
@@ -57,12 +73,14 @@ def main():
                     if((not costBuildingText.isdigit()) or (nameBuilding.isdigit())):
                         costBuildingText = "0.0"
                     costBuilding = float(costBuildingText)
-
-                    if((costBuilding < which_product_to_click[1]) and (costBuilding != 0.0)):
+                    if(which_product_to_click[1] < 0.0):
+                        which_product_to_click[1] = costBuilding
+                    if((costBuilding <= which_product_to_click[1]) and (costBuilding != 0.0)):
                         which_product_to_click[1] = costBuilding
                         which_product_to_click[0] = nameBuilding
                         
                 print("I am going to buy:",which_product_to_click[0])
+                print("The price of the product is:",which_product_to_click[1])
                 try:
                     driver.find_element(By.XPATH, '//*[@id="buy'+which_product_to_click[0]+'"]').click()
                 except:
@@ -86,7 +104,7 @@ def main():
                     number_of_purchases_per_eval_counter = 0
                     which_product_to_click[1] = cookies_per_cps
                     for x in range(0, 5):
-                        eval_value = evaluate_building(str(x))
+                        eval_value = evaluate_building(str(x),driver)
                         print("The eval value:",eval_value)
                         print("The value in list",which_product_to_click[1])
                         if((eval_value < which_product_to_click[1]) and (eval_value != 0.0)):
@@ -110,7 +128,7 @@ def main():
                 clicks = 0
         driver.quit()
 
-def evaluate_building(prod_numb):
+def evaluate_building(prod_numb,driver):
     try:
         element = driver.find_element(By.XPATH, '//*[@id="product'+prod_numb+'"]')
         webdriver.ActionChains(driver).move_to_element(element).perform()
@@ -124,7 +142,7 @@ def evaluate_building(prod_numb):
         pass
     return 0.0
 
-def evaluate_building2(prod_numb):
+def evaluate_building2(prod_numb,driver):
     try:
         current_Cursor_Price = float(driver.find_element(By.XPATH, '//*[@id="productPrice'+prod_numb+'"]').get_attribute("outerText").replace(",", "")) # gets the current price
     except:
